26375dcd660b0e75c15b06ca661dc62ed0e4d494
Genna Walsh <gennawalsh@gmail.com>
2018-10-19 15:37:49 +0100

    app.js handles submit of new entry

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-
-
-
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', handleFormSubmit);
 
 });
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  // grab values
+  const commonName = this.common.value;
+  const sciName = this.scientific.value;
+  const status = this.status.value;
+
+  // create container div for new addition
+  const newListItem = document.createElement('div');
+  newListItem.classList.add('animal-added');
+  const list = document.querySelector('.list-container');
+  list.appendChild(newListItem);
+
+  // create child elements within div
+  const itemHeader = document.createElement('p');
+  itemHeader.classList.add('name');
+  newListItem.appendChild(itemHeader);
+  const itemLatin = document.createElement('p');
+  itemLatin.classList.add('latin');
+  newListItem.appendChild(itemLatin);
+  const itemStatus = document.createElement('p');
+  itemStatus.classList.add('status');
+  newListItem.appendChild(itemStatus);
+
+  // input values
+  itemHeader.textContent = `${ commonName }`;
+  itemLatin.textContent = `${ sciName }`;
+  itemStatus.textContent = `${ status }`
+
+  // reset form
+  this.reset();
+
+  // prevent submit if field missing
+};
+
+// empty form after submit
